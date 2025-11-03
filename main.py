f50a354bf1f685957d482fc0ede25fa4e150efed
Manthan Awgan <manthanawgan@gmail.com>
2025-11-03 14:33:57 +0530

    67

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from pathlib import Path
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, refine_landmarks=True)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
+
 
 output_dir = Path("output")
 output_dir.mkdir(exist_ok=True)
 
+
 #GIF paths
 tongue_gif = "assets/tongue.gif"
 closed_eyes_gif = "assets/closed_eyes.gif"
+six_seven_gif = "assets/six_seven.gif"
 
 #Load GIFs
 def load_gif(path):
@@ -27,6 +32,7 @@ def load_gif(path):
 
 tongue_frames = load_gif(tongue_gif)
 eyes_frames = load_gif(closed_eyes_gif)
+hands_frames = load_gif(six_seven_gif)
 
 def eye_aspect_ratio(landmarks, eye_indices):
     p1, p2, p3, p4, p5, p6 = [np.array([landmarks[i].x, landmarks[i].y]) for i in eye_indices]
@@ -44,8 +50,20 @@ def mouth_aspect_ratio(landmarks):
     mar = np.linalg.norm(top - bottom) / np.linalg.norm(left - right)
     return mar
 
+def detect_hand_gesture(hand_landmarks):
+    thumb_tip = np.array([hand_landmarks.landmark[4].x, hand_landmarks.landmark[4].y])
+    index_tip = np.array([hand_landmarks.landmark[8].x, hand_landmarks.landmark[8].y])
+
+    distance = np.linalg.norm(thumb_tip - index_tip)
+
+    return distance < 0.1   #if dist is small enough then consider it is a specifis gesture(maybe i'll change it later)
+
+
+
 EYE_AR_THRESH = 0.20
 MOUTH_AR_THRESH = 0.55
+
+
 
 cap = cv2.VideoCapture(0)
 frames_for_gif = []
@@ -85,6 +103,16 @@ while True:
             reaction_mode = None
             cv2.putText(frame, "Normal", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
 
+    hand_results = hands.process(rgb)
+    if hand_results.multi_hand_landmarks:
+        for hand_landmarks in hand_results.multi_hand_landmarks:
+            if detect_hand_gesture(hand_landmarks):
+                reaction_mode = "hands"
+                cv2.putText(frame, "six sevennn", (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+
+
+
+
     #Display main camera feed
     cv2.imshow("Freak Detector", frame)
 
@@ -97,6 +125,10 @@ while True:
         gif_frame = tongue_frames[reaction_index % len(tongue_frames)]
         cv2.imshow("Reaction", gif_frame)
         reaction_index += 1
+    elif reaction_mode == "hands" and hands_frames:
+        gif_frame = hands_frames[reaction_index % len(hands_frames)]
+        cv2.imshow("Reaction", gif_frame)
+        reaction_index += 1
     else:
         blank = np.zeros((200, 300, 3), dtype=np.uint8)
         cv2.putText(blank, "Not Freaky", (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (200,200,200), 2)
@@ -107,3 +139,13 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+"""
+Main Loop:
+Captures webcam frame
+Detects face landmarks
+Checks for specific expressions (closed eyes, tongue out, juggling_hand[67])
+Shows appropriate reaction GIF based on detection
+
+"""
